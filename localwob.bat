2dbf15a1dea320dfdb74aaa24a503b38db17c01c
Moorea Tv <moorea@ymail.com>
2021-01-01 13:20:32 -0800

    use start for last step of local build test, also ignore update file

--- localwob.bat
+++ localwob.bat
@@ -2,4 +2,4 @@ copy ..\WowOpenBox\WowOpenBox.tkapp OpenMultiBoxing.vfs\app\
 tclkit\tclkit.exe  sdx/sdx.kit wrap OpenMultiBoxing -runtime runtime.exe
 del OpenMultiBoxing.exe
 rename OpenMultiBoxing OpenMultiBoxing.exe
-OpenMultiBoxing.exe -debug
+start .\OpenMultiBoxing.exe -debug
